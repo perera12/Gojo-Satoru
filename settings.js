7edc404a036fb68909b4540cdbefb01387cc8912
perera12 <109908218+perera12@users.noreply.github.com>
2022-07-24 19:18:23 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['94781135364']
+global.premium = ['94781135364']
+global.ownernomer = '94781135364'
 global.ownername = '🐦Star Boy Nexus'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
